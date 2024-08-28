e1145d7d9f9b6705ce1dd30ed54dbdaaf632fa3e
tsabudh <tsabudh@gmail.com>
2024-08-28 18:44:02 +0545

    chore: added sections to page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,17 @@
 import React, { FC } from "react";
 
-import Navbar from "../components/Navbar.tsx";
 import HomeSectionWorks from "../components/HomeSectionWorks.tsx";
 import HomeSectionHero from "../components/HomeSectionHero.tsx";
+import HomeSectionAbout from "../components/HomeSectionAbout.tsx";
+import HomeSectionTimeline from "../components/HomeSectionTimeline.tsx";
 
 const HomePage: FC = function HomePage() {
   return (
     <React.Fragment>
       <HomeSectionHero />
       <HomeSectionWorks />
+      <HomeSectionAbout />
+      <HomeSectionTimeline />
     </React.Fragment>
   );
 };
--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Cursor from "../components/Cursor";
 
 function Pages() {
   return (
     <React.Fragment>
+      <Cursor />
       <Navbar />
       <Outlet />
     </React.Fragment>
