90f7b7756405e9dcc7b4b17255de03f9ae5c33ac
tsabudh <tsabudh@gmail.com>
2024-09-28 14:58:47 +0545

    Chore: added a contact section in homepage with contact form

---
+++ src/components/HomeSectionContact.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import classNames from "classnames/bind";
+
+import styles from "./HomeSectionContact.module.scss";
+import ContactForm from "./ContactForm";
+
+const cx = classNames.bind(styles);
+
+function HomeSectionContact() {
+  return <section className={cx("section-contact")}>
+            <h2 className={cx("h2")}>Have a business or an idea?</h2>
+
+    <ContactForm/>
+  </section>;
+}
+
+export default HomeSectionContact;
