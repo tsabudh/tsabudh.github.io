557b48104e5c0e74080759c580010dffb41601a1
Sabudh Thapa <tsabudh@gmail.com>
2026-05-31 17:08:04 +0545

    chore: CNAME copy

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
 
@@ -8,6 +9,11 @@ const __dirname = dirname(__filename);
 export default defineConfig({
   base: "/", // GitHub Pages root (tsabudh.github.io)
   root: ".", // project root
+  plugins: [
+    viteStaticCopy({
+      targets: [{ src: "CNAME", dest: "." }],
+    }),
+  ],
   build: {
     outDir: "_site", // Eleventy’s output folder
     emptyOutDir: true, // clean before build
